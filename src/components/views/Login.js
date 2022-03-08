ff9ff1ca61078f0d0d2034f94294ab6b62a6a0fc
softconrob <71380532+softconrob@users.noreply.github.com>
2022-03-08 12:01:34 +0100

    changed login and registration

--- src/components/views/Login.js
+++ src/components/views/Login.js
@@ -40,6 +40,24 @@ const Login = props => {
   const [password, setPassword] = useState(null);
   const [username, setUsername] = useState(null);
 
+  const doRegistration = async () => {
+    try {
+      const requestBody = JSON.stringify({username, password});
+      const response = await api.post('/users', requestBody);
+
+      // Get the returned user and update a new object.
+      const user = new User(response.data);
+
+      // Store the token into the local storage.
+      localStorage.setItem('token', user.token);
+
+      // Login successfully worked --> navigate to the route /game in the GameRouter
+      history.push(`/game`);
+    } catch (error) {
+      alert(`Something went wrong during the Registration: \n${handleError(error)}`);
+    }
+  };
+
   const doLogin = async () => {
     try {
       const requestBody = JSON.stringify({username, password});
@@ -72,11 +90,20 @@ const Login = props => {
             value={password}
             onChange={n => setPassword(n)}
           />
-          <div className="login button-container">
+          <div className="registration button-container">
             <Button
               disabled={!username || !password}
-              width="100%"
-              onClick={() => doLogin()}
+              width="50%"
+              onClick={() => doRegistration()}
+            >
+              Registration
+            </Button>
+          </div>
+          <div className="login button-container">
+            <Button
+                disabled={!username || !password}
+                width="50%"
+                onClick={() => doLogin()}
             >
               Login
             </Button>
